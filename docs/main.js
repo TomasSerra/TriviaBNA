7e243b4c4703b4e1e3e93b810324e4735f38ea7b
Tomas Serra <tserra@MacBook-Air-de-Tomi.local>
2022-01-14 22:57:48 -0300

    primer boceto

---
+++ docs/main.js
@@ -0,0 +1,132 @@
+var r1box1 = document.getElementById("r1box1");
+var r1box2 = document.getElementById("r1box2");
+var r1box3 = document.getElementById("r1box3");
+var r2box1 = document.getElementById("r2box1");
+var r2box2 = document.getElementById("r2box2");
+var r2box3 = document.getElementById("r2box3");
+
+var pregunta1 = document.getElementById("pregunta1");
+var pregunta2 = document.getElementById("pregunta2");
+
+var termino = false;
+var gano = false;
+var numAleatorio = 0;
+
+window.onload = function() 
+{
+    start();
+};
+
+function start()
+{
+    numAleatorio = Math.floor(Math.random() * (2 - 0)) + 0; //Numero entre 0 y 1
+    if(numAleatorio == 0)
+    {
+        pregunta1.style.display = "block";
+        pregunta2.style.display = "none";
+    }
+    else if(numAleatorio == 1)
+    {
+        pregunta1.style.display = "none";
+        pregunta2.style.display = "block";
+    }
+}
+
+function correcta(numero)
+{
+    if(termino == false)
+    {
+        if(numero == 1)
+        {
+            r1box1.style.backgroundColor = '#1e8540';
+            r1box1.style.fontSize = "28px";
+            r1box1.style.padding = "13px 18px"
+            r1box1.innerHTML = "✓";
+        }
+        else if(numero == 2)
+        {
+            r1box2.style.backgroundColor = '#1e8540';
+            r1box1.style.fontSize = "28px";
+            r1box2.style.padding = "13px 18px"
+            r1box2.innerHTML = "✓";
+        }
+        else if(numero == 3)
+        {
+            r1box3.style.backgroundColor = '#1e8540';
+            r1box3.style.fontSize = "28px";
+            r1box3.style.padding = "13px 18px"
+            r1box3.innerHTML = "✓";
+        }
+        else if(numero == 4)
+        {
+            r2box1.style.backgroundColor = '#1e8540';
+            r2box1.style.fontSize = "28px";
+            r2box1.style.padding = "13px 18px"
+            r2box1.innerHTML = "✓";
+        }
+        else if(numero == 5)
+        {
+            r2box2.style.backgroundColor = '#1e8540';
+            r2box2.style.fontSize = "28px";
+            r2box2.style.padding = "13px 18px"
+            r2box2.innerHTML = "✓";
+        }
+        else if(numero == 6)
+        {
+            r2box3.style.backgroundColor = '#1e8540';
+            r2box3.style.fontSize = "28px";
+            r2box3.style.padding = "13px 18px"
+            r2box3.innerHTML = "✓";
+        }
+
+        numero = numero.toString();
+        document.getElementById(numero).style.backgroundColor = '#31de6b';
+        gano = true;
+        termino = true;
+
+        setTimeout(() => {
+            window.location.href="./futbolTexto.html";
+          }, 2000);
+    }
+}
+
+function incorrecta(numero)
+{
+    if(termino == false)
+    {
+        if(numero == 1)
+        {
+            r1box1.style.backgroundColor = '#851f1b';
+            r1box1.innerHTML = "×";
+        }
+        else if(numero == 2)
+        {
+            r1box2.style.backgroundColor = '#851f1b';
+            r1box2.innerHTML = "×";
+        }
+        else if(numero == 3)
+        {
+            r1box3.style.backgroundColor = '#851f1b';
+            r1box3.innerHTML = "×";
+        }
+        else if(numero == 4)
+        {
+            r2box1.style.backgroundColor = '#851f1b';
+            r2box1.innerHTML = "×";
+        }
+        else if(numero == 5)
+        {
+            r2box2.style.backgroundColor = '#851f1b';
+            r2box2.innerHTML = "×";
+        }
+        else if(numero == 6)
+        {
+            r2box3.style.backgroundColor = '#851f1b';
+            r2box3.innerHTML = "×";
+        }
+        numero = numero.toString();
+        document.getElementById(numero).style.backgroundColor = '#de3731';
+        gano = false;
+        termino = true;
+    }
+}
